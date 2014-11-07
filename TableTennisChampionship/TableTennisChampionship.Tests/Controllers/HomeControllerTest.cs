636da70ede71d85f019ace435b907f13cc3155ff
IvoAtanasov <ivoatanasov85@gmail.com>
2014-11-07 10:54:24 +0200

    Error fix

--- TableTennisChampionship/TableTennisChampionship.Tests/Controllers/HomeControllerTest.cs
+++ TableTennisChampionship/TableTennisChampionship.Tests/Controllers/HomeControllerTest.cs
@@ -4,8 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using TableTennisChampionship;
-using TableTennisChampionship.Controllers;
+using TableTennisChampionshipMain;
+using TableTennisChampionshipMain.Controllers;
 
 namespace TableTennisChampionship.Tests.Controllers
 {
