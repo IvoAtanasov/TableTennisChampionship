1d09d75883351ff5015b547e0e61227c7e412d4f
Ivo Ivanov Atanasov <ivoatanasov85@gmail.com>
2014-11-07 02:05:14 +0200

    match rules

--- TableTennisChampionship/TableTennisChampionship/Models/MatchRule.cs
+++ TableTennisChampionship/TableTennisChampionship/Models/MatchRule.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
-
 namespace TableTennisChampionship.Models
 {
-    public class MatchRules
+    public class MatchRule
     {
+        [Key]
+        public int MatchRuleID { get; set; }
+        [Display(Name="Точки за гейм")]
+        public int PointsPerGame { get; set; }
     }
 }
