42369ae10c9e450e0b300c4f86c60b19acbba400
Ivo Ivanov Atanasov <ivoatanasov85@gmail.com>
2015-01-19 22:25:26 +0200

    tournamentPlayer

---
+++ TableTennisChampionship/TableTennisChampionshipMain/ViewModels/TournamentPlayerInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace TableTennisChampionshipMain.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.ComponentModel.DataAnnotations;
+    public class TournamentPlayerInfo
+    {
+        [Required]
+        public int PlayerID { get; set; }
+        public int Rank { get; set; }
+        public int Points { get; set; }
+    }
+}
